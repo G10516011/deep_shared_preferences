8cc8db800714d74a8b4438cd477a063920e1224c
hihioscar <jinnia81936@gmail.com>
2021-01-22 11:43:23 +0800

    插件修改name: deep_shared_preferences

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:deep_shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
